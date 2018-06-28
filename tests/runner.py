2ed1cd52037f2d3f20929c8964a3eeb676911a0b
Matthew Addis <matthew.addis@arkivum.com>
2018-06-28 14:22:26 +0100

    split out property, added test and example

--- tests/runner.py
+++ tests/runner.py
@@ -33,6 +33,10 @@ class AbstractSchemaValidatorTest(object):
             "http://www.parcore.org/schema/types.json/#",
             "schemas/types.json"
         ),
+                (
+            "http://www.parcore.org/schema/par_property.json/#",
+            "schemas/par_property.json"
+        ),
     ]
 
     dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -85,12 +89,19 @@ class ToolTest(AbstractSchemaValidatorTest, TestCase):
     def runTest(self):
         self.validate_json('examples/md5sum.json')
 
+class PropertyTest(AbstractSchemaValidatorTest, TestCase):
+    def get_json_schema_file_name(self):
+        return 'schemas/par_property.json'
+
+    def runTest(self):
+        self.validate_json('examples/md5prop.json')
 
 def suite():
     suite = TestSuite()
     suite.addTest(FormatTest())
     suite.addTest(PreservationActionTest())
     suite.addTest(ToolTest())
+    suite.addTest(PropertyTest())
     return suite
 
 

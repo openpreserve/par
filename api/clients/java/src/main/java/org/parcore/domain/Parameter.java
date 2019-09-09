a990983218518c50afbb111c77f3f0bcd94d238a
Jack O'Sullivan <10220473+jackdos@users.noreply.github.com>
2019-09-09 14:13:12 +0100

    Added details for parameter object

--- api/clients/java/src/main/java/org/parcore/domain/Parameter.java
+++ api/clients/java/src/main/java/org/parcore/domain/Parameter.java
@@ -1,5 +1,8 @@
 package org.parcore.domain;
 
+import com.google.gson.annotations.SerializedName;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.Objects;
 
 /**
@@ -17,6 +20,42 @@ import java.util.Objects;
 @SuppressWarnings("unused")
 public class Parameter {
 
+    @SerializedName("description")
+    private String description = null;
+
+    @SerializedName("type")
+    private String type = null;
+
+    @SerializedName("value")
+    private String value = null;
+
+    @ApiModelProperty(value = "")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @ApiModelProperty(value = "")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @ApiModelProperty(value = "")
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
     @Override
     public boolean equals(java.lang.Object o) {
         if (this == o) {
@@ -25,12 +64,15 @@ public class Parameter {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        return true;
+        Parameter parameter = (Parameter) o;
+        return Objects.equals(this.description, parameter.description) &&
+                Objects.equals(this.type, parameter.type) &&
+                Objects.equals(this.value, parameter.value);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash();
+        return Objects.hash(description, type, value);
     }
 
 
@@ -39,6 +81,9 @@ public class Parameter {
         StringBuilder sb = new StringBuilder();
         sb.append("class Parameter {\n");
 
+        sb.append("    description: ").append(toIndentedString(description));
+        sb.append("    type: ").append(toIndentedString(type));
+        sb.append("    value: ").append(toIndentedString(value));
         sb.append("}");
         return sb.toString();
     }
